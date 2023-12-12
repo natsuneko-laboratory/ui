7f896059547566ec53ad14529a8416117c74b0aa
MOCHIZUKI Natsune <me@natsuneko.cat>
2023-12-12 19:22:22 +0900

    chore: add storysource and a11y

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,9 +3,11 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-links",
+    "@storybook/addon-storysource",
+    "@storybook/addon-a11y",
     "storybook-dark-mode",
   ],
   framework: {
